8b60403f0a44156cefe5307b50bf1022765d2413
bobbiosamantha <bobbiosam@gmail.com>
2022-11-15 19:53:46 -0500

    menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hey! Welcome to my Website!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
